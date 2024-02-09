79f3b77099224978070e7754e17b9955e27d2768
LEO <141844230+M-LEO-H@users.noreply.github.com>
2024-02-09 17:32:36 +0100

    Finishing the project
    The majority of the project was done without github, because of the error with the classrom link. Therefore a timeline of the project is lacking.

--- src/main/resources/static/ordering.js
+++ src/main/resources/static/ordering.js
@@ -1,51 +1,65 @@
-let purchases = []
-
-
-
-
-function checkFName(){
-    if (document.getElementById("fName").value === ""){
-        document.getElementById("fNameCheck").innerText = "First name cannot be empty";
-        document.getElementById("fName").value = "";
-    }
-}
-
-
+let purchases = [];
+let validation = [];
 
 
 function buyTicket(){
     const movie = document.getElementById("movie").value;
-    const ticketNumber = document.getElementById("ticketNr").value;
-    const fName = document.getElementById("email").value;
+    const ticketNumber = Number(document.getElementById("ticketNr").value);
+    const fName = document.getElementById("fName").value;
     const lName = document.getElementById("lName").value;
     const phoneNr = document.getElementById("phoneNr").value;
     const email = document.getElementById("email").value;
 
+    //Resetting the error messages
+    document.getElementById("movieCheck").innerText = "";
+    document.getElementById("ticketNrCheck").innerText = "";
+    document.getElementById("fNameCheck").innerText = "";
+    document.getElementById("lNameCheck").innerText = "";
+    document.getElementById("phoneNrCheck").innerText = "";
+    document.getElementById("emailCheck").innerText = "";
 
+    //Long validation of all inputs :(
     if (movie === "Select a movie"){
         document.getElementById("movieCheck").innerText = "Select a movie";
         document.getElementById("movie").value = "Select a movie";
-    }
+        validation.push(false);
+    }else validation.push(true);
+
     if (isNaN(ticketNumber) ){
         document.getElementById("ticketNrCheck").innerText = "Number of tickets is invalid";
         document.getElementById("ticketNr").value = "0";
+        validation.push(false);
     }else if(!Number.isInteger(ticketNumber) || ticketNumber <= 0){
         document.getElementById("ticketNrCheck").innerText = "Number of tickets is to small or not a whole number";
         document.getElementById("ticketNr").value = "0";
-    }
+        validation.push(false);
+    }else validation.push(true);
+
+    if (fName === ""){
+        document.getElementById("fNameCheck").innerText = "First name cannot be empty";
+        document.getElementById("fName").value = "";
+        validation.push(false);
+    }else validation.push(true);
 
     if (lName === ""){
         document.getElementById("lNameCheck").innerText = "Last name cannot be empty";
         document.getElementById("lName").value = "";
-    }
+        validation.push(false);
+    }else validation.push(true);
+
     if (phoneNr === ""){
         document.getElementById("phoneNrCheck").innerText = "Phone number cannot be empty";
         document.getElementById("phoneNr").value = "";
-    }
+        validation.push(false);
+    }else validation.push(true);
+
     if (email === "" || !email.includes("@")){
         document.getElementById("emailCheck").innerText = "Email cannot be empty and/or are not valid";
         document.getElementById("email").value = "";
-    }
+        validation.push(false);
+    }else validation.push(true);
+
+
 
     const purchase = {
         movieTitle : movie,
@@ -55,27 +69,41 @@ function buyTicket(){
         phoneNumber : phoneNr,
         email : email
     }
+    for (let i = 0; i < validation.length; i++){
+        if (validation[i] === false){
+            reset();
+            validation.splice(0, validation.length);
+            return;
+        }
+    }
+
+    reset();
     purchases.push(purchase);
-    document.getElementById("movieCheck").value = "Select a movie";
-    document.getElementById("ticketNrCheck").value = "0";
-    document.getElementById("fNameCheck").value = "";
-    document.getElementById("lNameCheck").value = "";
-    document.getElementById("phoneNrCheck").value = "";
-    document.getElementById("emailCheck").value = "";
-    showTickets(); //Creating a new function of this for imagined future needs
 
+    //Printing out the tickets in Purchases
+    let ut = "<table><tr>" +
+        "<th>Movie</th><th>total tickets</th><th>First name</th><th>Last name</th><th>Phone number</th><th>email</th>" +
+        "</tr>";
+    for (let p of purchases){
+        ut+="<tr>";
+        ut+="<td>"+p.movieTitle+"</td><td>"+p.ticketAmount+"</td><td>"+p.firstName+"</td><td>"+p.lastName+"</td><td>"+p.phoneNumber+"</td><td>"+p.email+"</td>";
+        ut+="</tr>";
+    }
+    document.getElementById("all tickets").innerHTML=ut;
+}
 
+//Reset function of all inputs
+function reset(){
+    document.getElementById("movie").value = "Select a movie";
+    document.getElementById("ticketNr").value = "0";
+    document.getElementById("fName").value = "";
+    document.getElementById("lName").value = "";
+    document.getElementById("phoneNr").value = "";
+    document.getElementById("email").value = "";
 }
 
 
-function showTickets(){
-        let ut = "<table><tr>" +
-            "<th>Movie</th><th>total tickets</th><th>First name</th><th>Last name</th><th>Phone number</th><th>email</th>" +
-            "</tr>";
-        for (let p of purchases){
-            ut+="<tr>";
-            ut+="<td>"+p.movieTitle+"</td><td>"+p.ticketAmount+"</td><td>"+p.firstName+"</td><td>"+p.lastName+"</td><td>"+p.phoneNumber+"</td><td>"+p.email+"</td>";
-            ut+="</tr>";
-        }
-        document.getElementById("all tickets").innerHTML=ut;
+function deleteTickets(){
+    purchases.splice(0,purchases.length);
+    document.getElementById("all tickets").innerHTML="";
 }
